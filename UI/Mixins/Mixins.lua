1c675e1493a7f28ad2225f1a0a276b621f3984f1
Alex Iukuridze <32624715+Aeon234@users.noreply.github.com>
2026-09-01 12:50:24 -0700

    feat(ui): add font string mixin

--- UI/Mixins/Mixins.lua
+++ UI/Mixins/Mixins.lua
@@ -3,5 +3,7 @@ local AMT = select(2, ...)
 ---@class AMTMixins
 ---@field Bar AMTBarMixin
 ---@field NewBar fun(parent: Frame): AMTBarMixin
+---@field Text AMTTextMixin
+---@field NewText fun(parent: Frame, layer: DrawLayer?): AMTTextMixin
 local Mixins = {}
 AMT.Mixins = Mixins
---
+++ UI/Mixins/Text.lua
@@ -0,0 +1,69 @@
+local AMT = select(2, ...)
+
+---@alias AMTFontOutline
+---| ""
+---| "OUTLINE"
+---| "THICKOUTLINE"
+---| "MONOCHROME"
+---| "MONOCHROME, OUTLINE"
+---| "MONOCHROME, THICKOUTLINE"
+---| "SLUG"
+---| "SLUG, OUTLINE"
+---| "SLUG, THICKOUTLINE"
+
+---@class AMTTextStyle
+---@field font string LSM font name
+---@field size number
+---@field outline AMTFontOutline
+---@field color number[] {r, g, b, a}
+---@field shadowColor number[]?
+---@field shadowOffset number[]? {x, y}
+---@field justify "LEFT"|"CENTER"|"RIGHT"?
+
+---@class AMTTextMixin : FontString
+local Text = {}
+AMT.Mixins.Text = Text
+
+---@param style AMTTextStyle
+function Text:ApplyStyle(style)
+	local font = AMT.Media.Font(style.font)
+
+	if font then
+		self:SetFont(font, style.size, style.outline)
+	end
+
+	local color = style.color
+	self:SetTextColor(color[1], color[2], color[3], color[4])
+
+	local shadow = style.shadowColor
+
+	if shadow then
+		local offset = style.shadowOffset
+
+		self:SetShadowColor(shadow[1], shadow[2], shadow[3], shadow[4])
+		self:SetShadowOffset(offset and offset[1] or 0, offset and offset[2] or 0)
+	else
+		self:SetShadowColor(0, 0, 0, 0)
+	end
+
+	if style.justify then
+		self:SetJustifyH(style.justify)
+	end
+end
+
+---@param format string
+---@param ... any
+function Text:SetFormatted(format, ...)
+	self:SetText(format:format(...))
+end
+
+---@param parent Frame
+---@param layer DrawLayer?
+---@return AMTTextMixin
+function AMT.Mixins.NewText(parent, layer)
+	local text = parent:CreateFontString(nil, layer or "OVERLAY")
+
+	Mixin(text, Text)
+
+	return text
+end
